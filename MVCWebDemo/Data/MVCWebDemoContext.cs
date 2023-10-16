318062d9e193aba84f3eb4baeabaf885f6c79cca
Predrag Jevtic <designbyheart@gmail.com>
2023-10-16 15:57:08 +0200

    Inital project

---
+++ MVCWebDemo/Data/MVCWebDemoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCWebDemo.Models;
+
+namespace MVCWebDemo.Data
+{
+    public class MVCWebDemoContext : DbContext
+    {
+        public MVCWebDemoContext (DbContextOptions<MVCWebDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCWebDemo.Models.User> User { get; set; } = default!;
+    }
+}
